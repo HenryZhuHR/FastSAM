1e8be4080b56596e7bc1aafe3da6333070cf9cf2
HenryZhuHR <296506195@qq.com>
2024-04-16 20:31:32 +0800

    ✨ feat: update install script

--- scripts/init-env.sh
+++ scripts/init-env.sh
@@ -1,29 +1,45 @@
 VENV_NAME=fastsam
+# CUSTOM_PYTHON_VERSION=3.10 # Uncomment and set to the desired Python version
+CUDA_VERSION=11.1
 
 if [ ! -d downloads/CLIP ]; then
     echo "Run 'bash scripts/get-resource.sh' first to download the CLIP repository."
     exit 1
 fi
 
-ENV_DIR=.env
+PROJECT_HOME=$(pwd)
+ENV_DIR=$PROJECT_HOME/.env
 
 if [ ! -d $ENV_DIR/$VENV_NAME ]; then
-    SYS_PYTHON_VERSION=$(python3 -c 'import sys; print(".".join(map(str, sys.version_info[:2])))')
-    conda create -p .env/$VENV_NAME -y python=$SYS_PYTHON_VERSION 
+    SYS_PYTHON_VERSION=$(python3 -c 'import sys; print(".".join(map(str, sys.version_info[:3])))')
+    if [ -z "$CUSTOM_PYTHON_VERSION" ]; then
+        ENV_PYTHON_VERSION=$SYS_PYTHON_VERSION
+    else
+        ENV_PYTHON_VERSION=$CUSTOM_PYTHON_VERSION
+    fi
+    conda create -p .env/$VENV_NAME -y python=$ENV_PYTHON_VERSION 
 else
     echo "Conda environment '$VENV_NAME' already exists."
 fi
 
+ENV_PATH=$ENV_DIR/$VENV_NAME
 
-echo "Activating conda environment '$VENV_NAME'..."
-
+echo "Activating conda environment '$ENV_PATH'..."
 eval "$(conda shell.bash hook)"
-conda activate $VENV_NAME
+conda activate $ENV_PATH
+echo "Activated $(python --version) in ($ENV_PATH)"
 
-echo "Activated $(python --version) in ($ENV_DIR/$VENV_NAME)"
-
+# CUDA_VERSION
 python3 -m pip install torch torchvision --index-url https://download.pytorch.org/whl/cu121
-python3 -m pip install -r requirements.txt
+# python3 -m pip install -r requirements.txt
 
+pip check -r requirements.txt
 
 python3 -m pip install -e downloads/CLIP
+
+echo "To activate this environment, use"
+echo "    conda activate $ENV_PATH"
+echo "To deactivate an active environment, use"
+echo "    conda deactivate"
+echo "To remove this environment, use"
+echo "    conda remove -n $VENV_NAME --all -y"
